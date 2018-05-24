ed470c24ce06ef9cf7013841d4a9cbbc10e989cd
Tom Hsu <thomaschsu@gmail.com>
2018-05-24 00:59:21 -0400

    Modified index and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+// Add 4 characters with HP / Attack Power / Counter-attack Power
+$("#characters").html("<p>Character1</p>");
+// Player selects one of the 4 characters. That character is moved to your character section
+// Enemies are moved to enmies available to attack
+// Player clicks on enemy, enemy is moved to defender area
+// Player clicks attack button, opponent loses HP
+// Opponent counter attacks
+// If reduced to zero or below, remove enemy from defender area, and choose new enemy
+// If lose, display game over
+// Player wins game by defeating all enemies
+});
