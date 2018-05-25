8b98377098b109fe0fa25d192d49daf7de88a8f8
Tom Hsu <thomaschsu@gmail.com>
2018-05-25 12:05:19 -0400

    Modified js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 // Add 4 characters with HP / Attack Power / Counter-attack Power
-$("#characters").html("<p>Character1</p>");
-// Player selects one of the 4 characters. That character is moved to your character section
-// Enemies are moved to enmies available to attack
+$("#characters").append("<div class='char-container'><p class='char-title'>Hodor the Great</p><img src='assets/images/char1.jpg'></div>");
+$("#characters").append("<div class='char-container'><p class='char-title'>Hodor the Great</p><img src='assets/images/char1.jpg'></div>");
+$("#characters").append("<div class='char-container'><p class='char-title'>Tyrion the Imp</p><img src='assets/images/char2.jpg'></div>");
+$("#characters").append("<div class='char-container'><p class='char-title'>Night King</p><img src='assets/images/char3.jpg'></div>");
+$("#characters").append("<div class='char-container'><p class='char-title'>Olly the POS</p><img src='assets/images/char4.jpg'></div>");
+// Player on click selects one of the 4 characters. That character is then moved to your character section
+// Enemies are moved to enemies available to attack
 // Player clicks on enemy, enemy is moved to defender area
 // Player clicks attack button, opponent loses HP
 // Opponent counter attacks
