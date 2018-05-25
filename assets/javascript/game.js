72bd1ab95283ffa8bbb1793224cd5541d854a62c
Tom Hsu <thomaschsu@gmail.com>
2018-05-25 17:00:57 -0400

    Modified js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,18 @@
+var hpChar1 = 100;
+var hpChar2 = 100;
+var hpChar3 = 100;
+var hpChar4 = 100;
+
 $(document).ready(function(){
 // Add 4 characters with HP / Attack Power / Counter-attack Power
-$("#characters").append("<div class='char-container'><p class='char-title'>Hodor the Great</p><img src='assets/images/char1.jpg'></div>");
-$("#characters").append("<div class='char-container'><p class='char-title'>Hodor the Great</p><img src='assets/images/char1.jpg'></div>");
-$("#characters").append("<div class='char-container'><p class='char-title'>Tyrion the Imp</p><img src='assets/images/char2.jpg'></div>");
-$("#characters").append("<div class='char-container'><p class='char-title'>Night King</p><img src='assets/images/char3.jpg'></div>");
-$("#characters").append("<div class='char-container'><p class='char-title'>Olly the POS</p><img src='assets/images/char4.jpg'></div>");
+$("#character-1").append("<div class='char-container'><p class='char-title'>Hodor the Greatest</p><img src='assets/images/char1.jpg'></div>");
+$("#character-1").after(hpChar1);
+$("#character-2").append("<div class='char-container'><p class='char-title'>Tyrion the Imp</p><img src='assets/images/char2.jpg'></div>");
+$("#character-2").after(hpChar2);
+$("#character-3").append("<div class='char-container'><p class='char-title'>Night King</p><img src='assets/images/char3.jpg'></div>");
+$("#character-3").after(hpChar3);
+$("#character-4").append("<div class='char-container'><p class='char-title'>Olly the Traitor</p><img src='assets/images/char4.jpg'></div>");
+$("#character-4").after(hpChar4);
 // Player on click selects one of the 4 characters. That character is then moved to your character section
 // Enemies are moved to enemies available to attack
 // Player clicks on enemy, enemy is moved to defender area
