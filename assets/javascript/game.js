2b17b9058a891b25dfea92168014bead7d9fc93c
Tom Hsu <thomaschsu@gmail.com>
2018-05-28 19:14:31 -0400

    Modified JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
-
+// Random number is a number 19 - 120
+var randomNumber = Math.floor(Math.random() * (120 - 19) ) + 19;
+console.log(randomNumber);
 
 $(document).ready(function(){
-
+    $("#random-number").append(randomNumber);
 });
