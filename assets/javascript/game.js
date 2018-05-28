c17f452d631e0548fee219b24fee8bd1873826d1
Tom Hsu <thomaschsu@gmail.com>
2018-05-28 19:43:28 -0400

    Modified JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,16 @@
-// Random number is a number 19 - 120
-var randomNumber = Math.floor(Math.random() * (120 - 19) ) + 19;
-console.log(randomNumber);
+// Variables
+var score = 0;
+var crystals = $("#crystals");
 
+// Main Random number is a number 19 - 120
+var randomNumber = Math.floor(Math.random() * (120 - 19) ) + 19;
+
+// Crystal Random number is a number 1-12
+var crystalNumber = Math.floor(Math.random() * (12 - 1) ) + 1;
+
+// jQuery
 $(document).ready(function(){
-    $("#random-number").append(randomNumber);
+    $("#random-number").text(randomNumber);
+    $("#total").text('Your total score is:');
+    $("#score").text(score);
 });
