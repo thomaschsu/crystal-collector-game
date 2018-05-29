318b5a8ebd4a233b5b80daeb15d421d5979bba92
Tom Hsu <thomaschsu@gmail.com>
2018-05-28 21:35:33 -0400

    Modified JS and HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,10 @@
-// Each time click on crystal, add the points to the total score
-// Goal of game is to match the randomNumber
-// If score matches the randomNumber, you get a win
-// If score goes over the randomNumber, you get a loss
 // Game resets with new numbers afterwards
-
 // Variables
-var score = 0;
 var crystals = $("#crystals");
 var crystalNumberArray = [];
+var score = 0;
+var win = 0;
+var loss = 0;
 
 // Functions
 
@@ -25,32 +22,41 @@ var crystalNumber4 = Math.floor(Math.random() * (12 - 1)) + 1;
 
 // jQuery
 $(document).ready(function() {
-            // Add randomNumber to page
-            $("#random-number").text(randomNumber);
+    $("#total").text('Your total score: ' + score);
+    // Add randomNumber to page
+    $("#random-number").text(randomNumber);
 
-            // 4 crystals, each one has a random number but number is hidden
-            for (var i = 0; i < 4; i++) {
-                var imageCrystal = $("<img>");
-                imageCrystal.addClass("crystal-image");
-                imageCrystal.attr("src", "assets/images/crystal-1.jpg");
-                crystalNumberArray.push(crystalNumber1, crystalNumber2, crystalNumber3, crystalNumber4);
-                imageCrystal.attr("data-crystalvalue", crystalNumberArray[i]);
-                crystals.append(imageCrystal);
-            }
+    // 4 crystals, each one has a random number but number is hidden
+    for (var i = 0; i < 4; i++) {
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "assets/images/crystal-1.jpg");
+        crystalNumberArray.push(crystalNumber1, crystalNumber2, crystalNumber3, crystalNumber4);
+        imageCrystal.attr("data-crystalvalue", crystalNumberArray[i]);
+        crystals.append(imageCrystal);
+    }
 
-            crystals.on("click", ".crystal-image", function(){
-                var crystalValue = ($(this).attr("data-crystalvalue"));
-                    crystalValue = parseInt(crystalValue);
-                    score += crystalValue;
-                    console.log(score);
+    $("#win").text('Wins: ' + win);
+    $("#losses").text('Losses: ' + loss);
 
-                    if (score === randomNumber) {
-                        alert("You win!");
-                    } else if (score >= randomNumber) {
-                        alert("You lose!!");
-                    }
-            })
+    // Each time crystal is clicked, add the points to the total score
+    crystals.on("click", ".crystal-image", function() {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
 
-            $("#total").text('Your total score is:');
-            $("#score").text(score);
-        });
+        // Update score with crystalValue
+        score += crystalValue;
+        $("#total").text('Your total score: ' + score);
+
+        // If score matches the randomNumber, you get a win
+        if (score === randomNumber) {
+            win++;
+            $("#win").text('Wins: ' + win);
+        
+        // If score goes over the randomNumber, you get a loss
+        } else if (score >= randomNumber) {
+            loss++;
+            $("#losses").text('Losses: ' + loss);
+        }
+    })
+});
